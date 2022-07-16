5c392dca00b837b6fa6d96afb919d3644ed09a4e
Nick <nwdepatie@gmail.com>
2022-07-16 15:51:20 -0400

    Added widget container called user input widget that contains all widgets for inputting an assignment

--- ui/widgets/user_input_widget.py
+++ ui/widgets/user_input_widget.py
@@ -1,25 +1,43 @@
 from PyQt5.QtWidgets import (
-    QApplication,
-    QComboBox,
-    QListWidgetItem, 
     QWidget, 
-    QPushButton, 
-    QLabel, 
-    QLineEdit, 
-    QListWidget, 
-    QCompleter, 
-    QCalendarWidget,
-    QScrollBar,
-    QComboBox
+    QHBoxLayout,
+    QVBoxLayout
     )
-from PyQt5.QtGui import QColor, QIcon, QPainter
-from .date_input_widget
-from .assignment_name_input_widget
-from .priority_input_widget
-from .subject_input_widget
-from .time_input_widget
+from .date_input_widget import DateInputWidget
+from .assignment_name_input_widget import AssignmentNameInputWidget
+from .priority_input_widget import PriorityInputWidget
+from .subject_input_widget import SubjectInputWidget
+from .time_input_widget import TimeInputWidget
+
+
+class UserTextInputPanel(QWidget):
+    assignmentNameInput = AssignmentNameInputWidget()
+    subjectInput = SubjectInputWidget()
+    priorityInput = PriorityInputWidget()
+
+    def __init__(self):
+        super().__init__()
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.subjectInput)
+        layout.addWidget(self.assignmentNameInput)
+        layout.addWidget(self.priorityInput)
+        self.setLayout(layout)
+
 
 class UserInputWidget(QWidget):
-    def __init__():
+    textInputPanel = UserTextInputPanel()
+    dateInput = DateInputWidget()
+    timeInput = TimeInputWidget()
+
+    def __init__(self):
         super().__init__()
-        
+        leftLayout = QVBoxLayout()
+        leftLayout.addWidget(self.textInputPanel)
+        leftLayout.addWidget(self.dateInput)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addLayout(leftLayout)
+        mainLayout.addWidget(self.timeInput)
+
+        self.setLayout(mainLayout)
